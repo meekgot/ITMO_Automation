2aa0918078d899c47fd805d0b60e4835dc775f1c
meekgot <ChrispeenLink@gmail.com>
2024-12-01 13:00:26 +0300

    First Commit

--- python_trainings/Home_Work/5hw.py
+++ python_trainings/Home_Work/5hw.py
@@ -1,24 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-
-#поиск элемента
-text = driver.find_element(By.CSS_SELECTOR, 'input[id="user-name"]')
-if text is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
-
-text2 = driver.find_element(By.CSS_SELECTOR, 'input[id="password"]')
-if text2 is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
-
-button = driver.find_element(By.CSS_SELECTOR, 'input[class="submit-button btn_action"]')
-if button is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
+def Check_site():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    text = driver.find_element(By.CSS_SELECTOR, 'input[id="user-name"]')
+    text2 = driver.find_element(By.CSS_SELECTOR, 'input[id="password"]')
+    button = driver.find_element(By.CSS_SELECTOR, 'input[class="submit-button btn_action"]')
+    if text is None and text2 is None and button is None:
+        print('Элементы не найдены')
+    else:
+        print('Элементы найдены')
